113b37816126fe2197770647ad45998bec81ac40
Your Name <you@example.com>
2024-12-27 14:41:27 +0545

    value changes

--- pages/about/index.js
+++ pages/about/index.js
@@ -172,7 +172,7 @@ const About = () => {
               </div>
               <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0">
                 <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
-                  <AnimatedNumbers value={7} />+
+                  <AnimatedNumbers value={25} />+
                 </div>
                 <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">
                   Finished Projects
@@ -180,7 +180,7 @@ const About = () => {
               </div>
               <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0">
                 <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
-                  <AnimatedNumbers value={10} />+
+                  <AnimatedNumbers value={100} />+
                 </div>
                 <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">
                   Satisfied Clients
